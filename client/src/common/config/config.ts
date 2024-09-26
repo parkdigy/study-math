5aa8c1b7a0f8442ffb982967492ebeea9d8196f2
parkdigy <parkdigy@gmail.com>
2024-09-26 23:47:46 +0900

    save

--- client/src/common/config/config.ts
+++ client/src/common/config/config.ts
@@ -1,5 +1,6 @@
 const config = {
-  version: (window as any).$$AppConfig?.version,
+  version: (window as any).$$MyAppConfig?.version,
+  env: (window as any).$$MyAppConfig?.env,
 };
 
 export default config;
