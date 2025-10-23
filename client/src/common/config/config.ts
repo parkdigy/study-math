1741b6066441f5c9d453ac23049ba300f56cec92
parkdigy <parkdigy@gmail.com>
2025-10-23 15:55:02 +0900

    Update

--- client/src/common/config/config.ts
+++ client/src/common/config/config.ts
@@ -1,6 +1,29 @@
-const config = {
-  version: (window as any).$$MyAppConfig?.version,
-  env: (window as any).$$MyAppConfig?.env,
+const myAppConfig: Dict<string> = (window as any).$$MyAppConfig || {};
+
+const version = ifUndefined(myAppConfig.version, '');
+const env = ifUndefined(myAppConfig.env, 'local') as 'local' | 'development' | 'staging' | 'production';
+const isLocal = env === 'local';
+const isDevelopment = env === 'development';
+const isStaging = env === 'staging';
+const isProduction = env === 'production';
+const title = ifUndefined(myAppConfig.title, '');
+
+const config: {
+  version: string;
+  env: 'local' | 'development' | 'staging' | 'production';
+  isLocal: boolean;
+  isDevelopment: boolean;
+  isStaging: boolean;
+  isProduction: boolean;
+  title: string;
+} = {
+  version,
+  env,
+  isLocal,
+  isDevelopment,
+  isStaging,
+  isProduction,
+  title,
 };
 
 export default config;
