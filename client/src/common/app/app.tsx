f5dcd500e3f0e4783e5cc3efe2c72dc2fd341f4d
parkdigy <parkdigy@gmail.com>
2025-10-28 12:49:44 +0900

    Update

--- client/src/common/app/app.tsx
+++ client/src/common/app/app.tsx
@@ -1,3 +1,5 @@
+import { SetURLSearchParams } from 'react-router';
+
 const app = {
   /********************************************************************************************************************
    * Browser ID
@@ -41,6 +43,48 @@ const app = {
 
   scrollToTop(top = 0, smooth = false) {
     window.scrollTo({ left: 0, top, behavior: smooth ? 'smooth' : 'instant' });
+  },
+
+  /********************************************************************************************************************
+   * Hash
+   * ******************************************************************************************************************/
+
+  deHash(location?: Location) {
+    const values: Dict<string> = {};
+    const hash = ifUndefined(location, window.location).hash.substring(1);
+    hash.replace(/([^=&]+)=([^&]*)/g, (substring, key, value) => {
+      values[decodeURIComponent(key)] = decodeURIComponent(value);
+      return substring;
+    });
+    return values;
+  },
+
+  /********************************************************************************************************************
+   * SearchParams
+   * ******************************************************************************************************************/
+
+  updateSearchParams(setSearchParams: SetURLSearchParams, params: Dict<string | undefined>, reset = false) {
+    setSearchParams((prev) => {
+      if (reset) {
+        prev
+          .keys()
+          .toArray()
+          .forEach((key) => prev.delete(key));
+      }
+
+      Object.keys(params).forEach((key) => {
+        const value = params[key];
+        if (value !== undefined) {
+          if (value === '') {
+            prev.delete(key);
+          } else {
+            prev.set(key, String(value));
+          }
+        }
+      });
+
+      return prev;
+    });
   },
 };
 
